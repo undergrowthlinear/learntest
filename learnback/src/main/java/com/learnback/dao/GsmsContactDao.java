7d7328923c386827eb81e5cda7712fa69e4d7ee4
undergrowthlinear <undergrowth@126.com>
2016-01-08 09:05:06 +0800

    delete learnback

--- learnback/src/main/java/com/learnback/dao/GsmsContactDao.java
+++
@@ -1,8 +0,0 @@
-package com.learnback.dao;
-
-import com.learnback.entity.GsmsContact;
-
-public interface GsmsContactDao {
-	
-	GsmsContact selectByPrimaryKey(Integer id);
-}
--- learnback/src/main/java/com/learnback/service/GsmsContactService.java
+++
@@ -1,8 +0,0 @@
-package com.learnback.service;
-
-import com.learnback.entity.GsmsContact;
-
-public interface GsmsContactService {
-	
-	GsmsContact selectByPrimaryKey(Integer id);
-}
